f8984c64de7ee59e7a51552e7c1bc15bbec06cad
crisor <cristhian.roncancio.mt@usa.edu.co>
2022-10-10 15:05:42 -0500

    first commit

---
+++ src/main/java/com/example/tutoriar3/TutoriaR3Application.java
@@ -0,0 +1,13 @@
+package com.example.tutoriar3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoriaR3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutoriaR3Application.class, args);
+    }
+
+}
